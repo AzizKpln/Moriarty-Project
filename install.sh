736a549ddf476c68a5ce22d27b5c071fbf459a17
Aziz Kaplan <azizkaplan@protonmail.ch>
2020-05-09 08:02:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 sudo apt-get install chromium -y
 pip3 install colored
 pip3 install requests
-pip3 install selenium
-pip3 install phonenumbers
 wget https://chromedriver.storage.googleapis.com/81.0.4044.69/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 cp chromedriver path/
 rm chromedriver*
-pip3 install boto3
 pip3 install clint
