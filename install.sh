de5684ae008ddd1cbdfee226d1a8002e73ba1e41
Aziz Kaplan <azizkaplan@protonmail.ch>
2020-05-10 14:06:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 sudo apt-get update && sudo apt-get install python3-pip
 sudo apt-get install chromium -y
 pip3 install colored
-pip3 install requests
 wget https://chromedriver.storage.googleapis.com/81.0.4044.69/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 cp chromedriver path/
 rm chromedriver*
 pip3 install clint
 sudo apt-get install figlet
-pip3 install bs4
 pip3 install certifi
