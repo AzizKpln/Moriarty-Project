16608bc48215eacd97f463467a3029fdb9eb296f
Aziz Kaplan <aziz.kaplan@std.yeditepe.edu.tr>
2020-11-25 13:25:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 python3 banners/installation_banner.py
+cp chromedriver risks_and_deep_search/
+cp chromedriver social_media/
 sudo apt-get update && sudo apt-get install python3-pip -y
 sudo apt-get install chromium -y
 pip3 install colored
