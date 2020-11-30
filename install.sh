3886e949f15797ef74f721e06115bf67792905b6
Aziz Kaplan <AzizKpln@protonmail.com>
2020-11-30 09:51:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 python3 banners/installation_banner.py
 sudo apt-get update && sudo apt-get install python3-pip -y
 sudo apt-get install chromium -y
-pip3 install colored
 python3 path/chromedriver_installation.py
 unzip chromedriver_linux64.zip
 rm -r chromedriver_linux64.zip
@@ -9,5 +8,7 @@ cp chromedriver path/
 cp chromedriver risks_and_deep_search/
 cp chromedriver social_media/
 sudo apt-get install figlet
+pip3 install colored
+pip3 install boto3
 pip3 install -r requirements.txt
 
