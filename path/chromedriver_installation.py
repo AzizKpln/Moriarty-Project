49a46014b9db72829286a7cee2c6c9289911c403
Aziz Kaplan <AzizKpln@protonmail.com>
2021-05-03 09:05:43 -0400

    Moriarty

--- path/chromedriver_installation.py
+++ path/chromedriver_installation.py
@@ -1,4 +1,5 @@
-import requests,string,subprocess
+import requests,string,subprocess,re
+from bs4 import BeautifulSoup
 class auto_chromedriver_installer:
 	def __init__(self):
 		self.url_file = 'https://chromedriver.storage.googleapis.com/'
@@ -11,26 +12,21 @@ class auto_chromedriver_installer:
 					self.current_chromium_version=j
 	def chromedriver_version(self):
 		self.check_chromium()
-		cv=self.current_chromium_version.split(".")
-		if cv[0]=="81":
-			self.current_chromedriver_version="81.0.4044.138"
-		elif cv[0]=="83":
-			self.current_chromedriver_version="83.0.4103.39"
-		elif cv[0]=="84":
-			self.current_chromedriver_version="84.0.4147.30"
-		elif cv[0]=="85":
-			self.current_chromedriver_version="85.0.4183.87"
-		elif cv[0]=="86":
-			self.current_chromedriver_version="86.0.4240.22"
-		elif cv[0]=="87":
-			self.current_chromedriver_version="87.0.4280.20"
-		else:
-			self.current_chromedriver_version=requests.get("https://chromedriver.storage.googleapis.com/LATEST_RELEASE").text
-			
-	def install(self):
+		self.current_chromedriver_version=self.current_chromium_version.split(".")
+		self.current_chromedriver_version=self.current_chromedriver_version[0]+"."+self.current_chromedriver_version[1]+"."+self.current_chromedriver_version[2]
+		print(self.current_chromedriver_version)
+	def parseTheWeb(self):
 		self.chromedriver_version()
-		print(self.current_chromium_version)
+		r = requests.get('https://chromedriver.chromium.org/downloads')
+		source=BeautifulSoup(r.content,"html.parser")
+		self.findVersion=source.find("div").text
+		self.findVersion=self.findVersion.split(self.current_chromedriver_version)
+		self.get_version=re.search("\d\d",self.findVersion[1]).group(0)
+		self.current_chromedriver_version=self.current_chromedriver_version+"."+self.get_version
+	def install(self):
+		self.parseTheWeb()
 		file = requests.get(self.url_file + self.current_chromedriver_version + '/' + self.file_name)
+		print(self.url_file + self.current_chromedriver_version + '/' + self.file_name)
 		with open(self.file_name, "wb") as code:
 			code.write(file.content)
 
