73d352ca5ef855edd4a0a00c1294a1adb563fc85
Aziz Kaplan <AzizKpln@protonmail.com>
2022-02-11 12:42:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 python3 banners/installation_banner.py
 sudo apt-get update && sudo apt-get install python3-pip -y
 sudo apt-get install chromium -y
-echo $(chromium --version | cut -d "." -f 1 | sed -e 's/.*[^0-9]\([0-9]\+\)[^0-9]*$/\1/') > c_Version.moriarty
+cd Moriarty/ && echo $(chromium --version | cut -d "." -f 1 | sed -e 's/.*[^0-9]\([0-9]\+\)[^0-9]*$/\1/') > c_Version.moriarty && cd ../
 
 unzip chromedriver_linux64.zip
 rm -r chromedriver_linux64.zip
